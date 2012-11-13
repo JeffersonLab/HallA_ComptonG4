3b77abf88376d958d82525f14b536e2264956ac2
Juan Carlos Cornejo <cornejo@jlab.org>
2012-11-13 11:41:44 -0500

    Removed no longer used variables.

--- include/ComptonG4DetectorConstruction.hh
+++ include/ComptonG4DetectorConstruction.hh
@@ -4,11 +4,6 @@
 #include <G4VUserDetectorConstruction.hh>
 
 // Predefine classes
-class G4VPhysicalVolume;
-class G4Material;
-class G4Box;
-class G4LogicalVolume;
-class G4VPhysicalVolume;
 
 class ComptonG4DetectorConstruction : public G4VUserDetectorConstruction {
 
@@ -18,9 +13,6 @@ public:
 
   G4VPhysicalVolume* Construct();
 private:
-  G4Material*        fDefaultMaterial;
-  G4Box*             fSolidWorld;
-  G4LogicalVolume*   fLogicalWorld;
   G4VPhysicalVolume* fPhysicsWorld;
 };
 
