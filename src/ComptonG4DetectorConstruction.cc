021a826bc10875c82ff8a5725a433653106074f3
Juan Carlos Cornejo <cornejo@jlab.org>
2013-10-30 15:33:17 -0400

    Adding some debug output in preparation of sensative detectors

--- src/ComptonG4DetectorConstruction.cc
+++ src/ComptonG4DetectorConstruction.cc
@@ -9,8 +9,11 @@
 #include <G4PVPlacement.hh>
 #include <G4ThreeVector.hh>
 #include "G4LogicalVolumeStore.hh"
+#include "G4PhysicalVolumeStore.hh"
 #include <G4Colour.hh>
 #include <G4VisAttributes.hh>
+#include <G4SolidStore.hh>
+#include <G4VSolid.hh>
 
 // GEANT4 Geometry related includes
 #include <G4GDMLParser.hh>
@@ -33,10 +36,25 @@ ComptonG4DetectorConstruction::ComptonG4DetectorConstruction(
 
   // For visualization purposes, load and respect the color attributes
   const G4LogicalVolumeStore* lvs = G4LogicalVolumeStore::GetInstance();
+  const G4PhysicalVolumeStore* pvs = G4PhysicalVolumeStore::GetInstance();
+  const G4SolidStore* solids = G4SolidStore::GetInstance();
   std::vector<G4LogicalVolume*>::const_iterator lvciter;
+  std::vector<G4VPhysicalVolume*>::const_iterator pvciter;
+  std::vector<G4VSolid*>::const_iterator soliter;
   G4Colour color;
+  G4cout << "Parsing through solids" << G4endl;
+  for( soliter = solids->begin(); soliter != solids->end(); soliter++ ) {
+    G4cout << (*soliter)->GetName() << G4endl;
+  }
+
+  G4cout << "Parsing through physical volumes" << G4endl;
+  for( pvciter = pvs->begin(); pvciter != pvs->end(); pvciter++ ) {
+    G4cout << (*pvciter)->GetName() << G4endl;
+  }
+  G4cout << "Parsing through logical volumes" << G4endl;
   for( lvciter = lvs->begin(); lvciter != lvs->end(); lvciter++ )
   {
+    G4cout << (*lvciter)->GetName() << G4endl;
     G4GDMLAuxListType auxInfo = parser.GetVolumeAuxiliaryInformation(*lvciter);
     std::vector<G4GDMLAuxPairType>::const_iterator ipair = auxInfo.begin();
     for( ipair = auxInfo.begin(); ipair != auxInfo.end(); ipair++ )
@@ -44,69 +62,65 @@ ComptonG4DetectorConstruction::ComptonG4DetectorConstruction(
 
       G4String str=ipair->type;
       G4String val=ipair->value;
-      double red = 1.0;
-      double green = 1.0;
-      double blue = 1.0;
-      double alpha = 1.0;
-      std::stringstream ss;
-      unsigned int temp;
-      /*
-      G4cout << " Auxiliary Information is found for Logical Volume :  "
-        << (*lvciter)->GetName() << G4endl;
-      G4cout << " Name of Auxiliary type is     :  " << str << G4endl;
-      G4cout << " Associated Auxiliary value is :  " << val << G4endl;
-      */
-      // Check to see if it's not a know color
-      if(!G4Colour::GetColour(val,color)) {
-        // Sigh...okay, let's figure out the color. There are four possible
-        // combinations. They can either be numbers or hex values, and either
-        // one of these can include an alpha value or not.
-        // For now, assume only HEX values in the form of #AABBCC or if they
-        // have the alpha value #AABBCCDD
-        boost::to_lower(val);
-        boost::replace_all(val,"#","");
-        /*std::vector<std::string> results;
-        boost::split_regex(results,val,
+      if( str == "SensDet" ) {
+      } else if ( str == "Color" ) {
+        double red = 1.0;
+        double green = 1.0;
+        double blue = 1.0;
+        double alpha = 1.0;
+        std::stringstream ss;
+        unsigned int temp;
+        /*
+           G4cout << " Auxiliary Information is found for Logical Volume :  "
+           << (*lvciter)->GetName() << G4endl;
+           G4cout << " Name of Auxiliary type is     :  " << str << G4endl;
+           G4cout << " Associated Auxiliary value is :  " << val << G4endl;
+           */
+        // Check to see if it's not a know color
+        if(!G4Colour::GetColour(val,color)) {
+          // Sigh...okay, let's figure out the color. There are four possible
+          // combinations. They can either be numbers or hex values, and either
+          // one of these can include an alpha value or not.
+          // For now, assume only HEX values in the form of #AABBCC or if they
+          // have the alpha value #AABBCCDD
+          boost::to_lower(val);
+          boost::replace_all(val,"#","");
+          /*std::vector<std::string> results;
+            boost::split_regex(results,val,
             boost::regex("[a-f]"));*/
-        if(val.size() >= 6 ) {
-          G4cout << "Setting color settings for " << (*lvciter)->GetName() << G4endl;
-          G4cout << val.substr(0,2);
-          ss << std::hex << val.substr(0,2);
-          ss >> temp;
-          red = temp/256.;
-          ss.str("");
-          ss.clear();
-          G4cout << val.substr(2,2);
-          ss << std::hex << val.substr(2,2);
-          ss >> temp;
-          green = temp/256.;
-          ss.str("");
-          ss.clear();
-          G4cout << val.substr(4,2);
-          ss << std::hex << val.substr(4,2);
-          ss >> temp;
-          blue = temp/256.;
-          ss.str("");
-          ss.clear();
-          if(val.size() >= 8 ) {
-            G4cout << val.substr(6,2);
-            ss << std::hex << val.substr(6,2);
+          if(val.size() >= 6 ) {
+            ss << std::hex << val.substr(0,2);
             ss >> temp;
-            alpha = temp/256.;
+            red = temp/256.;
             ss.str("");
             ss.clear();
+            ss << std::hex << val.substr(2,2);
+            ss >> temp;
+            green = temp/256.;
+            ss.str("");
+            ss.clear();
+            ss << std::hex << val.substr(4,2);
+            ss >> temp;
+            blue = temp/256.;
+            ss.str("");
+            ss.clear();
+            if(val.size() >= 8 ) {
+              ss << std::hex << val.substr(6,2);
+              ss >> temp;
+              alpha = temp/256.;
+              ss.str("");
+              ss.clear();
+            }
           }
-          G4cout << G4endl;
-          G4cout << "red: " << red << G4endl;
-          G4cout << "green: " << green << G4endl;
-          G4cout << "blue: " << blue << G4endl;
-          G4cout << "alpha: " << alpha << G4endl;
+          color = G4Colour(red,green,blue,alpha);
         }
-        color = G4Colour(red,green,blue,alpha);
+        (*lvciter)->SetVisAttributes(new G4VisAttributes(color));
       }
-      (*lvciter)->SetVisAttributes(new G4VisAttributes(color));
     }
   }
+
+
+
 
 }
 
