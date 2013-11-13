ca18c5f38814c7d6534776cfa603783e31d344ef
Juan Carlos Cornejo <cornejo@jlab.org>
2013-11-13 13:13:17 -0500

    Ability to register sensitive detectors with the manager.

--- include/ComptonG4SensitiveDetectorManager.hh
+++ include/ComptonG4SensitiveDetectorManager.hh
@@ -9,18 +9,23 @@
 #define COMPTONG4SENSITIVEDETECTORMANAGER_HH_
 
 #include <vector>
+#include <G4String.hh>
 
 class ComptonG4Analysis;
 class ComptonG4SensitiveDetector;
+class G4VSensitiveDetector;
 
 class ComptonG4SensitiveDetectorManager {
 public:
   ComptonG4SensitiveDetectorManager(ComptonG4Analysis*);
   virtual ~ComptonG4SensitiveDetectorManager();
 
+  // Register new detector
+  G4VSensitiveDetector* RegisterDetector(G4String name);
+
 private:
   ComptonG4Analysis *fAnalysis;
-  std::vector<ComptonG4SensitiveDetector*> fDetector;
+  std::vector<ComptonG4SensitiveDetector*> fDetectors;
 };
 
 #endif /* COMPTONG4SENSITIVEDETECTORMANAGER_HH_ */
--- src/ComptonG4SensitiveDetectorManager.cc
+++ src/ComptonG4SensitiveDetectorManager.cc
@@ -6,6 +6,9 @@
  */
 
 #include "ComptonG4SensitiveDetectorManager.hh"
+#include "ComptonG4SensitiveDetector.hh"
+#include <G4INCLGeant4Compat.hh>
+#include <G4SDManager.hh>
 
 ComptonG4SensitiveDetectorManager::ComptonG4SensitiveDetectorManager(ComptonG4Analysis *analysis) :
     fAnalysis(analysis)
@@ -16,4 +19,14 @@ ComptonG4SensitiveDetectorManager::ComptonG4SensitiveDetectorManager(ComptonG4An
 ComptonG4SensitiveDetectorManager::~ComptonG4SensitiveDetectorManager()
 {
   // TODO Auto-generated destructor stub
+}
+
+
+G4VSensitiveDetector* ComptonG4SensitiveDetectorManager::RegisterDetector(G4String name)
+{
+  G4cout << "Registering new Sensitive Detector:  " << name << G4endl;
+  fDetectors.push_back(new ComptonG4SensitiveDetector(name));
+  G4SDManager* SDman = G4SDManager::GetSDMpointer();
+  SDman->AddNewDetector( fDetectors.back() );
+  return fDetectors.back();
 }
