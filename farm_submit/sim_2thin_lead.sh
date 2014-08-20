4ba3b7044bd7ef1ac4077f0a1773025527a020d6
Juan Carlos Cornejo <cornejo@jlab.org>
2014-08-20 14:38:25 -0400

    Adding in sumbission scripts for no and 2 thin leads

---
+++ farm_submit/sim_2thin_lead.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+for i in `seq 8001 8099`;
+do
+  cp sim_2thin_lead.mac pbs/sim_2thin_lead_${i}.mac
+  cp sim_2thin_lead.xml pbs/sim_2thin_lead_${i}.xml
+  sed -i -e 's/RUNNUM/'${i}'/g' -- pbs/sim_2thin_lead_$i.mac
+  sed -i -e 's/RUNNUM/'${i}'/g' -- pbs/sim_2thin_lead_$i.xml
+  jsub -xml pbs/sim_2thin_lead_$i.xml
+  #cat sim_2thin_lead_$i.xml
+  #rm sim_2thin_lead_$i.xml
+done
