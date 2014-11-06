d469a2a38e81bd8cb9c07a337d0668f8ae98ebde
Juan Carlos Cornejo <cornejo@jlab.org>
2014-11-06 10:03:10 -0500

    Clean up the no-longer used SteppingAction.  The class is left behind in case it needs to be used in the future.
    This also cleaned up a compiler warning about un-used variables.

--- src/ComptonG4SteppingAction.cc
+++ src/ComptonG4SteppingAction.cc
@@ -21,32 +21,6 @@ ComptonG4SteppingAction::~ComptonG4SteppingAction()
 {
 }
 
-void ComptonG4SteppingAction::UserSteppingAction(const G4Step* step)
+void ComptonG4SteppingAction::UserSteppingAction(const G4Step*)
 {
-  const G4Track* track = step->GetTrack();
-  G4VPhysicalVolume* volume = track->GetVolume();
-  G4ThreeVector pos(track->GetPosition());
-  /* G4cout << "@" << volume->GetName() << " \tcharge: " << track->GetDefinition()->GetPDGCharge()
-      << " \tParentID: " << track->GetParentID() << " \tThisID: " << track->GetTrackID() << " \tEnergy: "
-      << step->GetTotalEnergyDeposit()/MeV
-      << " \tPre Step Status: " << step->GetPreStepPoint()->GetStepStatus()
-      << " \tPre Step Energy: " << step->GetPreStepPoint()->GetTotalEnergy()/MeV
-      << " \tPost Step Status: " << step->GetPostStepPoint()->GetStepStatus()
-      << " \tPost Step Energy: " << step->GetPostStepPoint()->GetTotalEnergy()/MeV
-      << G4endl;*/
-  // TODO: The detector should not be hard coded!! Use the Active Detectors
-  // command to specify which detector to use
-  /*if (//track->GetDefinition()->GetPDGCharge() != 0. &&
-      !strcmp(volume->GetName(),"LeadTungstateCrystal_Logical_PV")) {
-    if(track->GetDefinition()->GetPDGCharge() != 0 ) {
-      fAnalysis->AddEDep(step->GetTotalEnergyDeposit()/MeV);
-      fAnalysis->AddStepSize(step->GetStepLength()/mm);
-    }
-    fAnalysis->AddEDepAllCharges(step->GetTotalEnergyDeposit()/MeV);
-    fAnalysis->AddStepSizeAllCharges(step->GetStepLength()/mm);
-
-  }// else {
-  //  std::cout << "At volume: " << volume->GetName() << " \tEDep: "
-  //      << step->GetTotalEnergyDeposit()/MeV << "\n";
-  //}*/
 }
