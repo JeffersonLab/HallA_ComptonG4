f90c6f7a537b7ce1dec82fd89f8307d7f174134f
Juan Carlos Cornejo <cornejo@jlab.org>
2014-12-10 00:29:05 -0500

    Ensure that the Hit data is cleared by the destructor

--- sources/hits/src/VComptonG4Hit.cc
+++ sources/hits/src/VComptonG4Hit.cc
@@ -12,6 +12,8 @@ VComptonG4Hit::VComptonG4Hit(VComptonG4Data *ptr) : fData(ptr)
  */
 VComptonG4Hit::~VComptonG4Hit()
 {
+  if(fData)
+    delete fData;
 }
 
 void VComptonG4Hit::Clone(const VComptonG4Hit* other)
