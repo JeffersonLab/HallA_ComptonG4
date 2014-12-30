0d3ea7c628de75232d059566ec42b099a904baf1
Juan Carlos Cornejo <cornejo@jlab.org>
2014-12-30 18:35:34 -0500

    Changed scint yield from 120/MeV to 150.8/MeV and the gain from 25% to 30%

--- tests/scintyield/scintyield.sh
+++ tests/scintyield/scintyield.sh
@@ -15,16 +15,18 @@ fi
 
 if [ "x$3" == "x" ];
 then
-  echo "Did not specify Setting number (1-3)"
+  echo "Did not specify Setting number (1-4)"
   exit;
 fi
 
-for i in `seq $1 $2`;
+for i in `seq -f %05g $1 $2`;
 do
-  cp ${prefix}.xml pbs/${prefix}_${3}_${i}.xml
-  sed -i -e 's/SETTING/'${i}'/g' -- pbs/${prefix}_${3}_${i}.xml
-  sed -i -e 's/RUNNUM/'${i}'/g' -- pbs/${prefix}_${3}_${i}.xml
-  #jsub -xml pbs/${prefix}_$i.xml
+  file_base="pbs/${prefix}_${3}_${i}"
+  xml_file="${file_base}.xml"
+  cp ${prefix}.xml ${xml_file}
+  sed -i -e 's/SETTING/'${3}'/g' -- ${xml_file}
+  sed -i -e 's/RUNNUM/'${i}'/g' -- ${xml_file}
+  jsub -xml ${xml_file}
   #cat ${prefix}_$i.xml
   #rm ${prefix}_$i.xml
 done
