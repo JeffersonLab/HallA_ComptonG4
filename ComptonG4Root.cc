ec41c5c79df819fd7877a17526c7199b3531bfb5
Juan Carlos Cornejo <cornejo@jlab.org>
2016-08-04 14:59:04 -0400

    Implement the last remaining steps so that we can fully support ROOT6.
    This now loads up either CINT or Cling libraries as needed, and the ROOT6
    dictionaries are also copied over to the install directory (they are *.pcm
    files).
    
    This closes #2.

--- ComptonG4Root.cc
+++ ComptonG4Root.cc
@@ -7,6 +7,7 @@
 #include <TString.h>
 #include <TRint.h>
 #include <cstdlib>
+#include <iostream>
 
 int main(int argc, char** argv)
 {
@@ -16,8 +17,15 @@ int main(int argc, char** argv)
   TString path = getenv("COMPTONG4");
   // So that it can work with the installed version too
   gROOT->ProcessLine(".include " + path + "/include");
-  gROOT->ProcessLine("gSystem->Load(\"libCint.so\");");
+  // Load either CINT for ROOT5 or less, and cling for ROOT 6 and above
+  if(gROOT->GetVersionInt()<60000) {
+    gROOT->ProcessLine("gSystem->Load(\"libCint.so\");");
+  } else {
+    gROOT->ProcessLine("gSystem->Load(\"libCling.so\");");
+  }
+  // Load the dynamic library for ComptonG4 (this adds all the dictionaries)
   gROOT->ProcessLine("gSystem->Load(\"libComptonG4.so\");");
+
   // Run the interface
   rint->Run();
   // Delete object
